c42f3c7995ebfe6a51d63a947325f4f5daf946d9
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-02 18:42:24 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- sample-assignment/sol.py
+++ sample-assignment/sol.py
@@ -1,10 +1,10 @@
-# Exercise: Assignment-1
+"""# Exercise: Assignment-1
 # Write a Python function, factorial(n), that takes in one number and
 # returns the factorial of given number.
-
 # This function takes in one number and returns one number.
-
+"""
 def main():
+	""" this is usefull to take input"""
     data = input()
     print(int(data[0])+int(data[1]))
 
