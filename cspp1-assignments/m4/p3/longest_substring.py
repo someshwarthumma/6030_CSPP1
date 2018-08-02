38ab739b816cc6aca5a043f1988bdeef4dff3a2d
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-02 20:46:37 +0530

    " -> 5 of 5 passed. pylint: 5.00/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -11,21 +11,21 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc.'''
 
 def main():
-	string = input()
-	end=0
-	highest=0
-	count=0
-	for var in range(len(string)-1):
-    if string[var]<= string[var+1]:
-        count += 1
-        if count > highest:
-            highest = count
-            end =  var + 1
-    else:
-        count=0
-	num= end-highest
-	print(string[num:end+1])
-	# the input string is in s
-	# remove pass and start your code here
+    string = input()
+    end=0
+    highest=0
+    count=0
+    for var in range(len(string)-1):
+        if string[var]<= string[var+1]:
+            count += 1
+            if count > highest:
+                highest = count
+                end =  var + 1
+        else:
+            count=0
+    num = end-highest
+    print(string[num:end+1])
+# the input string is in s
+# remove pass and start your code here
 if __name__ == "__main__":
-	main()
+    main()
