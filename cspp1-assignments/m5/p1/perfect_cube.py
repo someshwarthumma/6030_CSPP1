79cc258d7f7e3757b4670f8f2d8d428ed068f9ff
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-03 17:54:26 +0530

    " -> 2 of 4 passed. pylint: 6.92/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -1,16 +1,6 @@
-# Write a python program to find if the given number is a perfect cube or not
-# using guess and check algorithm
-
-# testcase 1
-# Input: 24389
-# Output: 24389 is a perfect cube
-
-# testcase 2
-# Input: 21950
-# Output: 21950 is not a perfect cube
-
+''' Write a python program to find if the given number is a perfect cube or not'''
 def main():
-    # input is captured in s
+    '''This program is to find wheather the given number is perfect cube or not'''
     given_number = int(input())
     guess = 0
     step = 1
@@ -19,11 +9,8 @@ def main():
             break
         guess = guess + step 
     if (guess**3 - given_number) == 0 :
-        print(str(guess)+" is a perfect cube")
+        print(str(given_number)+" is a perfect cube")
     else :
-        print(str(guess)+" not a perfect cube")
-    # watch out for the data type of value stored in s
-    # your code starts here
-
+        print(str(given_number)+" not a perfect cube")
 if __name__ == "__main__":
     main()
