eb2187f8bd2684583eedcd686e49e89693d285e9
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-04 11:45:15 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -2,21 +2,20 @@
 def main():
     '''To display the product of all the digits in a given number'''
     int_input = int(input())
-    flag=0
+    flag = 0
     if int_input < 0:
         flag = 1
-        str_input=str(int_input)
-        str_input=str_input[1:]
+        str_input = str(int_input)
+        str_input = str_input[1:]
     else:
-        str_input=str(int_input)
-        str_input=str_input[0:]
+        str_input = str(int_input)
+        str_input = str_input[0:]
     temp2 = 0
     temp1 = 1
-    #if input >= 1 then perform while loop 
     for var in str_input:
-        temp2=int(var)
+        temp2 = int(var)
         temp1 = temp1*temp2
-    if int_input == 0 :
+    if int_input == 0:
         print(temp2)
     else:
         if flag == 1:
