d1c317ba8a41f8b5760617ef4dbe930992cf26d5
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-06 22:50:33 +0530

    " -> 10 of 10 passed. pylint: 8.67/10 "

--- cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -4,16 +4,18 @@ def paying_debt_off_in_a_year(balance, annual_interest_rate):
         return 0
     monthly_rate = annual_interest_rate/12
     lower_bound = balance/12
-    upper_bound =(balance*(1 +monthly_rate)**12)/12
+    upper_bound = (balance*(1 +monthly_rate)**12)/12
     lowest_payment = (lower_bound+upper_bound)/2
     epsilon= 0.03
     while abs(balance) > epsilon:
         lowest_payment = (lower_bound+upper_bound)/2
         prev_bal = balance
-        for i in range(12):
+        var = 0
+        while var <= 11:
             monthly_balance = prev_bal - lowest_payment
             upd_bal = monthly_balance + (monthly_rate*monthly_balance)
             prev_bal = upd_bal
+            var = var+1
         if (monthly_balance) > epsilon:
             lower_bound = lowest_payment
         elif (monthly_balance) <- epsilon:
@@ -29,5 +31,5 @@ def main():
     data = list(map(float, data))
     print("Lowest Payment:", paying_debt_off_in_a_year(data[0], data[1]))
     
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
