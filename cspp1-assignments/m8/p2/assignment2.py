bac2d8b2f0e35c18ebe3ebe8124469d0543d1caf
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-07 19:16:19 +0530

    " -> 1 of 10 passed. pylint: 2.22/10 "

--- cspp1-assignments/m8/p2/assignment2.py
+++ cspp1-assignments/m8/p2/assignment2.py
@@ -1,24 +1,22 @@
-# Exercise: Assignment-2
-# Write a Python function, sumofdigits, that takes in one number and returns the sum of digits of given number.
+'''# Exercise: Assignment-2
+# Write a Python function, sumofdigits
+that takes in one number and returns the sum of digits of given number.
+'''
 
-# This function takes in one number and returns one number.
-
-
-def sumofdigits(n):
+def sum_of_digits(num):
     '''
     n is positive Integer
-
     returns: a positive integer, the sum of digits of n.
     '''
-    if n <= 0:
-    	return 0
-    return n%10+sumofdigits(n//10)
+    if num <= 0:
+        return 0
+    return num%10+sumofdigits(num//10)
 
 
 def main():
-    a = input()
-    print(sumofdigits(int(a)))  
+    num = input()
+    print(sum_of_digits(int(num)))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
 
