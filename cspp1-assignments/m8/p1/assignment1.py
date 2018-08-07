f2adf095a4d4491c71a65caefa0136b622bc55bb
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-07 13:52:39 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -1,19 +1,19 @@
 '''# Exercise: Assignment-1
 # Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
 # This function takes in one number and returns one number.'''
-def factorial(n):
+def factorial(fact):
     '''
     n is positive Integer
     returns: a positive integer, the factorial of n.
     '''
-    if n == 0:
+    if fact == 0:
         return 1
     else:
-        return n*factorial(n-1)
+        return fact*factorial(fact-1)
 def main():
     '''This function is used to find factorial of number using recursion'''
-    a = input()
-    print(factorial(int(a)))    
+    fact = input()
+    print(factorial(int(fact))) 
 
 if __name__ == "__main__":
     main()
