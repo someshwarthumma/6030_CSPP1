780abf34e19c0af17c2f18f01ac6a5491fa59b0d
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-07 13:55:05 +0530

    " -> 10 of 10 passed. pylint: 7.78/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -1,6 +1,7 @@
 '''# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
-# This function takes in one number and returns one number.'''
+# Write a Python function, factorial(n), that takes in one number
+and returns the factorial of given number.'''
+
 def factorial(fact):
     '''
     n is positive Integer
