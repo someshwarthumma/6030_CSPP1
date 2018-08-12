ebd92cfc416105aab728e357abaabbd800d39530
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-12 15:06:03 +0530

    " -> 0 of 2 passed. pylint: 7.14/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -14,8 +14,8 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
 
-    if arg2 in list(network.keys()):
-        network[arg2].append(arg1)
+    if arg1 in list(network.keys()):
+        network[arg1].append(arg2)
     else:
         network[arg2]=[arg1]
     return network
