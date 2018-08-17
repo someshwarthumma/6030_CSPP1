53b659beec1ddba8df5a300547c51f3df14caf94
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-17 18:11:30 +0530

    " -> 0 of 3 passed. pylint: 8.20/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -41,12 +41,15 @@ def clean_up_words(input_file):
             input_file=input_file.replace(i, '')
     input_file = input_file.lower()
     input_file = input_file.split()
+    print(input_file)
     if '' in input_file:
         input_file.remove('')
+    print(input_file)
     return input_file
 
 def remove_stopwords(input_list, filename):
-    for i in input_list:
+    temp = input_list[:]
+    for i in temp:
         if i in filename:
             input_list.remove(i)
     return input_list
