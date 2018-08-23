394fdc6c72fc583e823772f66761b5371291fb9f
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-23 15:50:39 +0530

    " -> 7 of 8 passed. pylint: 9.31/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -95,7 +95,7 @@ def main():
         # multiply matrix 1 and matrix 2
         multiplication = mult_matrix(matrix1, matrix2, dimensions1, dimensions2)
         print(multiplication)
-    except Exception as e:
-        print(e)
+    except Exception as Error:
+        print(Error)
 if __name__ == '__main__':
     main()
