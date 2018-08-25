ed81eace418d29c1e1a0d0222ebd065a4aa6e49c
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 11:31:02 +0530

    " -> 4 of 4 passed. pylint: 9.00/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -5,25 +5,32 @@ each word
 import re
 
 def tokenize(string):
-	dictionary = {}
-	for list_ in string:
-		for word in list_:
-			if word in dictionary.keys():
-				dictionary[word] = dictionary[word] + 1
+    '''
+    :param input is a list of list 
+    return a dictionary with word frequency
+    '''
+    dictionary = {}
+    for list_ in string:
+        for word in list_:
+            if word in dictionary.keys():
+                dictionary[word] = dictionary[word] + 1
 
-			else:
-				dictionary[word] = 1
-	return dictionary
+            else:
+                dictionary[word] = 1
+    return dictionary
             
 def main():
-	string = []
-	no_of_lines = int(input())
-	for _ in range(no_of_lines):
-		temp_input = input()
-		regex = re.compile("[^A-Za-z0-9 ]")
-		temp_input = regex.sub('', temp_input)
-		string.append(temp_input.split())
-	print(tokenize(string))
+    '''
+    Initialisation for Tokenise the given string
+    '''
+    string = []
+    no_of_lines = int(input())
+    for _ in range(no_of_lines):
+        temp_input = input()
+        regex = re.compile("[^A-Za-z0-9 ]")
+        temp_input = regex.sub('', temp_input)
+        string.append(temp_input.split())
+    print(tokenize(string))
 
 
 if __name__ == '__main__':
