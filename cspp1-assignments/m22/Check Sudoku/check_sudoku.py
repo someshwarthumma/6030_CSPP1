5eb098e0b277259e82efc10f37367dcf05523c6a
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 12:51:32 +0530

    " -> 22 of 26 passed. pylint: 1.46/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -7,13 +7,62 @@
     Complete the check_sudoku function to check if the given grid
     satisfies all the sudoku rules given in the statement above.
 '''
+def sub_marix(sudoku, k, l):
+    sub = []
+    for i in range(3):
+        i = i + k
+        for j in range(3):
+            j = j + l
+            sub.append(sudoku[i][j])
+    return sub
+
+
+
+def transpose_of_matrix(sudoku):
+    temp_matrix = [['4', '3', '5', '2', '6', '9', '7', '10', '1'], 
+    ['6', '8', '2', '5', '7', '1', '4', '9', '3'], 
+    ['1', '9', '7', '8', '3', '4', '5', '6', '2'], 
+    ['8', '2', '6', '1', '9', '5', '3', '4', '7'], 
+    ['3', '7', '4', '6', '8', '2', '9', '1', '5'], 
+    ['9', '5', '1', '7', '4', '3', '6', '8', '2'], 
+    ['5', '1', '9', '3', '2', '6', '8', '7', '4'], 
+    ['2', '4', '8', '9', '5', '7', '1', '3', '6'], 
+    ['7', '6', '3', '4', '1', '8', '2', '5', '9']]
+    for i in range(9):
+        for j in range(9):
+            temp_matrix[j][i] = sudoku[i][j]
+    return temp_matrix
 
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    pass
+    transpose_matrix = transpose_of_matrix(sudoku)
+    for i in range(9):
+        if len(set(sudoku[i])) != 10:
+            return False
+        if len(set(transpose_matrix[i])) != 10:
+            return False
+        if len(set(sub_marix(sudoku,0,0))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,0,3))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,0,6))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,3,0))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,3,3))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,3,6))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,6,0))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,6,3))) != 10:
+            return False
+        if len(set(sub_marix(sudoku,6,6))) != 10:
+            return False
+        return True
 
 def main():
     '''
