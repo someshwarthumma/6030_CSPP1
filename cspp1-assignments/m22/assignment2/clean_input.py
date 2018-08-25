15d7852d9043ae247f2be3be8769dad16a69a92c
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 11:01:59 +0530

    " -> 3 of 3 passed. pylint: 5.56/10 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -5,13 +5,8 @@ alphabets in both upper and lower case and numbers.
 import re
 
 def clean_string(string):
-    new_string = ""
-    for character in string:
-        if character in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890":
-            new_string = new_string + character
-
-    return new_string
-
+    regex = re.compile("[^A-Za-z0-9]")
+    return regex.sub('',string)
 def main():
     string = input()
     print(clean_string(string))
