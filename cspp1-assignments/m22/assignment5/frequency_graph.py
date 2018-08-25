f6527757ebfd3c0b8f05db991e616802adaba285
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 11:58:37 +0530

    " -> 0 of 4 passed. pylint: 2.50/10 "

--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -2,9 +2,19 @@
 Write a function to print a dictionary with the keys in sorted order along with the
 frequency of each word. Display the frequency values using “#” as a text based graph
 '''
+def sub(temp):
+	string = ""
+	for _ in range(temp):
+		string = string + "#"
+	return string
 
 def frequency_graph(dictionary):
-    pass
+    
+    for key in dictionary:
+    	temp = dictionary[key]
+    	dictionary[key] = sub(temp)
+    for key in dictionary:
+    	print(key, "-", dictionary[key])
 
 def main():
     dictionary = eval(input())
