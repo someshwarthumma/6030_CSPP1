08df9ed12e50d10d206ca1958d0baa3bbf799ec6
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 12:06:17 +0530

    " -> 4 of 4 passed. pylint: 9.00/10 "

--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -13,20 +13,30 @@ def print_dictionary(dictionary):
         print(key, "-", dictionary[key])
 
 def sub(temp):
-	string = ""
-	for _ in range(temp):
-		string = string + "#"
-	return string
+    '''
+    To form a string with # such that it contains given number of # in the formed string
+    return a string of #
+    '''
+    string = ""
+    for _ in range(temp):
+        string = string + "#"
+    return string
 
 def frequency_graph(dictionary):
+    '''
+    To for the dictinary with appended # in place of value in the given dictionary
+    '''
     
     for key in dictionary:
-    	temp = dictionary[key]
-    	dictionary[key] = sub(temp)
+        temp = dictionary[key]
+        dictionary[key] = sub(temp)
     print_dictionary(dictionary)
 
 
 def main():
+    '''
+    Initialisation for forming the frequency graph and printing the same
+    '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 
