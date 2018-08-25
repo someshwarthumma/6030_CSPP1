ca8583ade2a9ab11b8f37b9db609912dc1693acd
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-25 10:56:54 +0530

    " -> 3 of 3 passed. pylint: 6.67/10 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -2,9 +2,15 @@
 Write a function to clean up a given string by removing the special characters and retain 
 alphabets in both upper and lower case and numbers.
 '''
+import re
 
 def clean_string(string):
-    pass
+    new_string = ""
+    for character in string:
+        if character in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890":
+            new_string = new_string + character
+
+    return new_string
 
 def main():
     string = input()
